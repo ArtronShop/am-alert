d89bf71b5ac23b301de391a58c5cae1f0c7d2f6e
Max <7087907+maxpromer@users.noreply.github.com>
2025-03-31 00:42:16 +0700

    Fixed

--- src/routes/api/room/[slug]/+server.ts
+++ src/routes/api/room/[slug]/+server.ts
@@ -1,13 +1,70 @@
 import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
+import type { RequestEvent, RequestHandler } from './$types';
+import { createNotification, getRoomById  } from '../../../../action';
+import type { roomsTable } from '../../../../db/schema';
 
-export const GET: RequestHandler = ({ request }) => {
-	// log all headers
-	console.log(...request.headers);
+const tokenValidate = (roomToken: string, authorization: string) => {
+    if (!authorization?.startsWith("Bearer ")) {
+        return false;
+    }
 
-	// create a JSON Response using a header we received
-	return json({
-		// retrieve a specific header
-		userAgent: request.headers.get('user-agent')
-	});
+    const authToken = authorization?.replace("Bearer ", "");
+    
+    return authToken === roomToken;
+}
+
+interface RequestEventWithRoomInfo extends RequestEvent {
+    roomInfo: typeof roomsTable.$inferSelect;
+}
+
+interface RequestHandlerWithRoomInfo {
+    (event: RequestEventWithRoomInfo): Promise<Response> | Response;
+}
+
+const withAuth = (next: RequestHandlerWithRoomInfo) => async (event: RequestEvent) => {
+    const { request, params} = event;
+    
+    const roomId = +params.slug;
+
+    const roomInfo = await getRoomById(roomId);
+    if (!roomInfo) {
+        return json({ error: "room id invalid" }, { status: 404 });
+    }
+
+    const authorization = request.headers.get("authorization") || "";
+    if (!tokenValidate(roomInfo?.token || "", authorization)) {
+        return json({ error: "invalid token" }, { status: 401 });
+    }
+
+    return next(({ ...event, roomInfo }) as RequestEventWithRoomInfo);
 };
+
+export const GET: RequestHandler = withAuth(async ({ roomInfo }) => {
+	return json(roomInfo);
+});
+
+interface AssetsProps {
+    image?: string;
+}
+
+export const POST: RequestHandler = withAuth(async ({ request, roomInfo }) => {
+    const { message, image }: { message: string; image?: string } = await request.json();
+    if (!message) {
+        return json({ error: "invalid 'message' object" }, { status: 401 });
+    }
+
+    let assets: AssetsProps = {};
+    if (image) {
+        assets.image = image;
+    }
+
+    const notificationId = createNotification({
+        roomId: roomInfo.id, 
+        message, 
+        assets
+    });
+
+    // TODO: send push notification to user
+
+	return json({ ok: true, notificationId });
+});
