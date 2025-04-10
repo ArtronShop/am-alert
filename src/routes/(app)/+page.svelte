5ba32cab68fa530f2b2ebf2b9acc948d1b2c68ca
Max <7087907+maxpromer@users.noreply.github.com>
2025-04-11 02:35:52 +0700

    Add join room work

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -9,12 +9,13 @@
     } from "flowbite-svelte";
 
     import {
-        PlusOutline,
+        ArrowRightOutline,
     } from "flowbite-svelte-icons";
 
     import type { PageProps } from './$types';
-
+    import { goto } from '$app/navigation';
     import { onMount } from 'svelte';
+    import type { RoomJoinListProps } from "../../types";
 
     let { data }: PageProps = $props();
     const { userInfo, roomList } = data;
@@ -22,7 +23,7 @@
     const handleClickAddRoom = () => {
         const addRoomIdInput = document.querySelector("#add-room-id") as HTMLInputElement;
         const roomId = +(addRoomIdInput?.value || 0);
-        alert(roomId);
+        goto("/room/" + roomId);
         addRoomIdInput.value = "";
     };
 
@@ -31,17 +32,18 @@
     onMount(async () => {
         let roomJoinListId: number[] = [];
         try {
-            roomJoinListId = JSON.parse(localStorage.getItem("RoomJoinList") || "[]");
+            const RoomJoinList: RoomJoinListProps = JSON.parse(localStorage.getItem("RoomJoinList") || "[]");
+            roomJoinListId = RoomJoinList.map(a => a.roomId);
         } catch(e) {
             // don't care
         }
 
+        const roomOwnerList = (roomList || [])?.map(a => a.id);
+        roomJoinListId = roomJoinListId.filter(a => !roomOwnerList.includes(a));
+        
         if(roomJoinListId.length === 0) {
             return;
         }
-
-        const roomOwnerList = (roomList || [])?.map(a => a.id);
-        roomJoinListId = roomJoinListId.filter(a => !roomOwnerList.includes(a));
 
         // Fetch room info
         const res = await fetch("/api/rooms?ids=" + roomJoinListId.join(","));
@@ -86,7 +88,7 @@
             placeholder="หมายเลขห้อง"
             class="grid-rows-1 mr-3"
         />
-        <Button on:click={handleClickAddRoom}><PlusOutline class="w-5 h-5" /></Button>
+        <Button on:click={handleClickAddRoom}><ArrowRightOutline class="w-5 h-5" /></Button>
     </div>
 
     <hr class="my-3 border-gray-300" />
